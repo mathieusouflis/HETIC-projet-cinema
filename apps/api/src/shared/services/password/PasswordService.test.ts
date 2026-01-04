7a3a0cd84fc7183b29490b82b36166324a73b47b
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-01-04 22:01:24 +0100

    tests(api): Add PasswordService tests to API

---
+++ apps/api/src/shared/services/password/PasswordService.test.ts
@@ -0,0 +1,20 @@
+import { describe } from "vitest"
+import { PasswordService } from "./PasswordService";
+
+describe("Password service test", () => {
+  const PASSWORD = "password";
+  const passwordService = new PasswordService();
+
+  it("should hash a password", async () => {
+    const hashedPassword = passwordService.hash(PASSWORD);
+    expect(hashedPassword).not.toEqual(PASSWORD);
+  });
+
+  it("should compare a password", async () => {
+    const hashedPassword = await passwordService.hash(PASSWORD);
+    let isMatch = await passwordService.compare(PASSWORD, "lala");
+    expect(isMatch).toBe(false);
+    isMatch = await passwordService.compare(PASSWORD, hashedPassword);
+    expect(isMatch).toBe(true);
+  });
+});
