e602febf727127f25c5889e8bf94f7a7196fca4b
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-01-05 11:21:51 +0100

    tests(api): Add login use case tests

---
+++ apps/api/src/modules/auth/application/use-cases/login.usecase.test.ts
@@ -0,0 +1,40 @@
+import { describe, it } from "vitest";
+import { createMockedUserRepository } from "../../../users/domain/interfaces/user.repository.mock";
+import { JWTService } from "../../../../shared/services/token";
+import { PasswordService } from "../../../../shared/services/password";
+import { LoginUseCase } from "./login.usecase";
+
+describe("LoginUseCase", () => {
+
+  const mockedUserRepository = createMockedUserRepository();
+  const tokenService = new JWTService();
+  const passwordService = new PasswordService();
+
+  const useCase = new LoginUseCase(mockedUserRepository, passwordService, tokenService)
+
+  it("should throw an error when user not found", async () => {
+    const email = "fakeEmail@gmail.com";
+    await expect(useCase.execute({ email, password: "fakePassword" })).rejects.toThrow();
+  });
+
+  it("should throw an error when wrong password", async () => {
+    const email = "test1@example.com";
+    const password = "fakePassword123";
+    await expect(useCase.execute({ email, password: password })).rejects.toThrow();
+  });
+
+  it("should throw error if user does not have password", async () => {
+    const email = "test3@example.com";
+    const password = "fakePassword123";
+    await expect(useCase.execute({ email, password: password })).rejects.toThrow();
+  });
+
+  it("should pass when email and password are correct", async () => {
+    const email = "test2@example.com";
+    const password = "Password123:)";
+
+    const loginResponse = await useCase.execute({ email, password })
+    expect(loginResponse).toBeDefined()
+  });
+
+});
