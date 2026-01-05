5aec93fe422c67bd58e3825a49b23f4dedab877a
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-01-05 12:18:23 +0100

    tests(api): Create GetUsers usecase test

---
+++ apps/api/src/modules/users/application/use-cases/GetUsers.usecase.test.ts
@@ -0,0 +1,17 @@
+import { describe, it } from "vitest";
+import { createMockedUserRepository } from "../../domain/interfaces/user.repository.mock";
+import { GetUsersUseCase } from "./GetUsers.usecase";
+
+describe("LoginUseCase", () => {
+
+  const mockedUserRepository = createMockedUserRepository();
+  const useCase = new GetUsersUseCase(mockedUserRepository)
+
+  it("Should get a list of users", async () => {
+    expect(await useCase.execute({
+      page: 1,
+      limit: 10,
+      offset: 0
+    })).toBeDefined();
+  });
+});
