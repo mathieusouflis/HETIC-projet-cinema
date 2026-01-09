4f91d32f6b46751f0078cea204fcf9930a26a4e4
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-01-09 11:46:19 +0100

    refactor(logger): Change log names

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,22 +1,19 @@
-export const info = (...args: unknown[]): void => {
-  console.log("\x1b[36m%s\x1b[0m", "[INFO]", ...args);
-};
+class Logger {
+  public info(...args: unknown[]): void {
+    console.log("\x1b[36m%s\x1b[0m", `    [INFO] `, ...args);
+  }
 
-export const warn = (...args: unknown[]): void => {
-  console.log("\x1b[33m%s\x1b[0m", "[WARN]", ...args);
-};
+  public warn(...args: unknown[]): void {
+    console.log("\x1b[33m%s\x1b[0m", `    [WARN] `, ...args);
+  }
 
-export const error = (...args: unknown[]): void => {
-  console.log("\x1b[31m%s\x1b[0m", "[ERROR]", ...args);
-};
+  public error(...args: unknown[]): void {
+    console.log("\x1b[31m%s\x1b[0m", `   [ERROR] `, ...args);
+  }
 
-export const success = (...args: unknown[]): void => {
-  console.log("\x1b[32m%s\x1b[0m", "[SUCCESS]", ...args);
-};
-
-export const logger = {
-  info: info,
-  warn: warn,
-  error: error,
-  success: success
+  public success(...args: unknown[]): void {
+    console.log("\x1b[32m%s\x1b[0m", ` [SUCCESS] `, ...args);
+  }
 }
+
+export const logger = new Logger()
