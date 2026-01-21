d6772caaeb3a1efceca550f4fa9316b60a2e80fe
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-01-21 21:48:31 +0100

    feat: Enforce tmdb_fetch_status constraints and errors

--- apps/api/src/modules/contents/infrastructure/database/repositories/tmdb-fetch-status/tmdb-fetch-status.repository.ts
+++ apps/api/src/modules/contents/infrastructure/database/repositories/tmdb-fetch-status/tmdb-fetch-status.repository.ts
@@ -1,6 +1,8 @@
 import { eq } from "drizzle-orm";
 import { db } from "../../../../../../database";
 import { tmdbFetchStatusSchema } from "../../schemas/tmdb-fetch-status.schema";
+import { MetadataNotFoundError } from "./errors/metadata-not-found";
+import { UnsupportedStatusTypeError } from "./errors/unsuported-status-type";
 
 type DiscoverMetadatas = {
   page: number;
@@ -13,24 +15,23 @@ export class TMDBFetchStatusRepository {
     await db.insert(tmdbFetchStatusSchema)
       .values({ path, type: "discover", metadata })
       .onConflictDoUpdate({
-        target: tmdbFetchStatusSchema.path,
+        target: [tmdbFetchStatusSchema.path],
         set: { metadata },
       });
   }
+    async getPathMetadatas(path: string): Promise<DiscoverMetadatas> {
+      const result = await db.query.tmdbFetchStatus.findFirst({
+        where: eq(tmdbFetchStatusSchema.path, path),
+      });
 
-  async getPathMetadatas(path: string): Promise<PathMetadata> {
-    const result = await db.query.tmdbFetchStatus.findFirst({
-      where: eq(tmdbFetchStatusSchema.path, path),
-    });
+      if (!result) {
+        throw new MetadataNotFoundError(path);
+      }
 
-    if (!result) {
-      throw new Error(`Path "${path}" not found`);
+      if (result.type !== "discover") {
+        throw new UnsupportedStatusTypeError(result.type);
+      }
+
+      return result.metadata as DiscoverMetadatas;
     }
-
-    if (result.type !== "discover") {
-      throw new Error(`Path type "${result.type}" not supported.`);
-    }
-
-    return result.metadata as DiscoverMetadatas;
   }
-}
