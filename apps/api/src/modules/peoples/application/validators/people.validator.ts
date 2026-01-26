a46c29390ddb7f1fc8d0858524049aee77ea35ae
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-01-27 00:09:55 +0100

    feat(people): add validator for people entity

---
+++ apps/api/src/modules/peoples/application/validators/people.validator.ts
@@ -0,0 +1,14 @@
+import z from "zod";
+import { uuidSchema } from "../../../../shared/schemas/fields";
+
+export const peopleValidator = z.object({
+  id: uuidSchema,
+  name: z.string().min(2).max(100),
+  bio: z.string().nullable(),
+  photoUrl: z.url().nullable(),
+  nationality: z.string().nullable(),
+  birthDate: z.date().nullable(),
+  tmdbId: z.number().int().nullable(),
+  createdAt: z.date(),
+  updatedAt: z.date()
+})
