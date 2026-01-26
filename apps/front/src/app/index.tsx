cd4f41c2d6b52afd4b25ba0434c18f4da239d16e
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-01-27 00:26:01 +0100

    refactor: Remove package ui from project

--- apps/front/src/app/index.tsx
+++ apps/front/src/app/index.tsx
@@ -1,6 +1,4 @@
 import "./styles.css";
-import { CounterButton } from "@packages/ui/counter-button";
-import { Link } from "@packages/ui/link";
 
 function App() {
   return (
@@ -9,16 +7,8 @@ function App() {
         Admin <br />
         <span>Kitchen Sink</span>
       </h1>
-      <CounterButton />
       <p className="description">
-        Built With{" "}
-        <Link href="https://turborepo.com" newTab>
-          Turborepo
-        </Link>
-        {" & "}
-        <Link href="https://vitejs.dev/" newTab>
-          Vite
-        </Link>
+        Built With ❤️
       </p>
     </div>
   );
