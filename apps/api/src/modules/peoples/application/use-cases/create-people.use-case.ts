23ccc404c7a13394e38b4025d278a92195481aaf
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-01-27 00:10:13 +0100

    feat(people): Implement create people use case

---
+++ apps/api/src/modules/peoples/application/use-cases/create-people.use-case.ts
@@ -0,0 +1,11 @@
+import { People, CreatePeopleProps } from "../../domain/entities/people.entity";
+import type { IPeoplesRepository } from "../../domain/interfaces/IPeoplesRepository";
+
+export class CreatePeopleUseCase {
+  constructor(private readonly peoplesRepository: IPeoplesRepository) {}
+
+  async execute(props: CreatePeopleProps): Promise<People> {
+    const people = await this.peoplesRepository.create(props);
+    return people;
+  }
+}
