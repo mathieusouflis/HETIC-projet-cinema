aeaba598cfd16936fcab39dbe1c33a8792f6e0bf
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-01-27 16:50:02 +0100

    feat(friendships): Move friendships table to user module

--- apps/api/src/database/schema.ts
+++ apps/api/src/database/schema.ts
@@ -22,6 +22,7 @@ import {
 import { contentSchema } from "../modules/contents/infrastructure/database/schemas/contents.schema";
 import { tmdbFetchStatusSchema } from "../modules/contents/infrastructure/database/schemas/tmdb-fetch-status.schema";
 import { peopleSchema } from "../modules/peoples/infrastructure/schemas/people.schema";
+import { friendshipsSchema } from "../modules/users/infrastructure/database/schemas/friendships.schema";
 import { users as usersSchema } from "../modules/users/infrastructure/database/schemas/users.schema";
 import {
   watchlistStatusEnum as watchlistCustomEnumImported,
@@ -73,47 +74,7 @@ export const refreshTokens = pgTable(
   ]
 );
 
-export const friendships = pgTable(
-  "friendships",
-  {
-    id: uuid().defaultRandom().primaryKey().notNull(),
-    userId: uuid("user_id").notNull(),
-    friendId: uuid("friend_id").notNull(),
-    status: varchar({ length: 20 }).default("pending").notNull(),
-    createdAt: timestamp("created_at", {
-      withTimezone: true,
-      mode: "string",
-    }).defaultNow(),
-    updatedAt: timestamp("updated_at", {
-      withTimezone: true,
-      mode: "string",
-    }).defaultNow(),
-  },
-  (table) => [
-    index("idx_friendships_friend").using(
-      "btree",
-      table.friendId.asc().nullsLast(),
-      table.status.asc().nullsLast()
-    ),
-    index("idx_friendships_user").using(
-      "btree",
-      table.userId.asc().nullsLast(),
-      table.status.asc().nullsLast()
-    ),
-    foreignKey({
-      columns: [table.userId],
-      foreignColumns: [users.id],
-      name: "friendships_user_id_fkey",
-    }).onDelete("cascade"),
-    foreignKey({
-      columns: [table.friendId],
-      foreignColumns: [users.id],
-      name: "friendships_friend_id_fkey",
-    }).onDelete("cascade"),
-    unique("unique_friendship").on(table.userId, table.friendId),
-    check("no_self_friend", sql`user_id <> friend_id`),
-  ]
-);
+export const friendships = friendshipsSchema;
 
 export const content = contentSchema;
 
