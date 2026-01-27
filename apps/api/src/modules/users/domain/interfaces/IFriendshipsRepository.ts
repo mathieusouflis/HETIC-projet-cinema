912be3aa80617e5931d093adac0aec5e86570d4f
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-01-27 21:19:54 +0100

    feat(friendship): Implement friendship repository

---
+++ apps/api/src/modules/users/domain/interfaces/IFriendshipsRepository.ts
@@ -0,0 +1,11 @@
+import type { FriendshipStatus } from "../../infrastructure/database/schemas/friendships.schema";
+import type { User } from "../entities";
+import type { Friendship } from "../entities/friendship.entity";
+
+export interface IFriendshipsRepository {
+  create(userId: string, friendId: string): Promise<Friendship>;
+  update(friendshipId: string, status: FriendshipStatus): Promise<Friendship>;
+  delete(friendshipId: string): Promise<void>;
+  getFollowers(userId: string): Promise<User[]>;
+  getFollowing(userId: string): Promise<User[]>;
+}
