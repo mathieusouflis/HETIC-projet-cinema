b89eb1adc405dc7231040d86115e9c799c07758f
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-01-28 10:34:59 +0100

    feat(tanstack-router): Setup tanstack router

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -1,6 +1,17 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+      routesDirectory: "./src/app",
+      generatedRouteTree: "./src/generated/routeTree.gen.ts",
+      routeFileIgnorePrefix: "-",
+      quoteStyle: "single"
+    }),
+    react()
+  ],
 });
