a05a6354a365eb12392d10d161b8cce4a9d5ffa3
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-02-04 21:17:29 +0100

    feat: Create content-platform entity

---
+++ apps/api/src/modules/content-platforms/domain/entities/content-platforms.entity.ts
@@ -0,0 +1,11 @@
+// import { Entity } from "../../../../shared/domain/entity";
+// import type {
+//   ContentPlatformsRow,
+//   contentPlatformsRelationsSchema,
+// } from "../../infrastructure/database/content-platforms.schema";
+
+// export class ContentPlatforms extends Entity<
+//   ContentPlatformsRow,
+//   typeof contentPlatformsRelationsSchema,
+//   {}
+// > {}
