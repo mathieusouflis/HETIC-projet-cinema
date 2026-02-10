60fc5b214f05f526cba56e39d620882d7aa8c39d
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-02-10 19:54:14 +0100

    feat(watchlist): add response DTO barrel export index

---
+++ apps/api/src/modules/watchlist/application/dto/response/index.ts
@@ -0,0 +1,5 @@
+export * from "./add-watchlist-content.response.validator.js";
+export * from "./get-watchlist.response.validator.js";
+export * from "./get-watchlist-content.response.validator.js";
+export * from "./patch-watchlist.response.validator.js";
+export * from "./query-watchlist.response.validator.js";
