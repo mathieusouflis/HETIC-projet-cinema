efce536765d3c60d8db066f74a8317c51ceb1bcd
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-02-10 19:54:31 +0100

    feat(watchparty): add response DTO barrel export index

---
+++ apps/api/src/modules/watchparty/application/dto/response/index.ts
@@ -0,0 +1,4 @@
+export * from "./create-watchparty.response.validator.js";
+export * from "./get-watchparty.response.validator.js";
+export * from "./query-watchparties.response.validator.js";
+export * from "./update-watchparty.response.validator.js";
