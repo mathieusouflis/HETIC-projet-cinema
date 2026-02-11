754801437425734a52d353bdb64a66fc8990ab4e
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-02-11 23:44:08 +0100

    feat: Implement categories api endpoint

--- apps/front/src/lib/api/services/index.ts
+++ apps/front/src/lib/api/services/index.ts
@@ -1,7 +1,9 @@
 import { authService } from "./auth";
 import { contentService } from "./contents";
+import { categorieService } from "./genres";
 
 export const getApi = () => ({
   auth: authService,
   contents: contentService,
+  categories: categorieService,
 });
