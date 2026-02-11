017341d20afe4e0d9469e45bfeb124a8d15a96c3
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-02-11 21:00:48 +0100

    feat: Add displayMovie Component

---
+++ apps/front/src/features/explore/components/display-movie.tsx
@@ -0,0 +1,49 @@
+import type { GETContents200DataItemsItem } from "@packages/api-sdk";
+import { Link } from "@tanstack/react-router";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { useRoutes } from "@/lib/routes";
+
+export interface DisplayMovieProps {
+  movie: GETContents200DataItemsItem;
+}
+
+export const DisplayMovie = (props: DisplayMovieProps) => {
+  const movie = props.movie;
+  const routes = useRoutes();
+
+  return (
+    <div className="relative flex flex-col items-end w-full aspect-13/6 ">
+      {movie.backdropUrl ? (
+        <div className="relative w-full h-full rounded-[28px] overflow-hidden">
+          <img
+            src={movie.backdropUrl}
+            alt={movie.title}
+            className="w-full h-full object-cover"
+          />
+          <div className="absolute inset-0 bg-linear-to-tr from-black/80 to-black/0" />
+        </div>
+      ) : (
+        <span className="w-full h-full object-cover rounded-[28px] bg-neutral-500" />
+      )}
+      <div className="absolute flex flex-col gap-10 bottom-12 left-12">
+        <span className="flex flex-col gap-5">
+          <div className="flex flex-wrap gap-1.5">
+            {movie.contentCategories?.map((category) => (
+              <Badge size={"lg"} variant={"glass"} key={category.id}>
+                {category.name}
+              </Badge>
+            ))}
+          </div>
+          <h2 className="text-6xl font-bold text-white">{movie.title}</h2>
+          <p className="text-white max-w-1/3 line-clamp-4">{movie.synopsis}</p>
+        </span>
+        <Link to={routes.contents.detail(movie.id)}>
+          <Button variant={"glass"} size={"2xl"} className="w-fit">
+            Discover {movie.type}
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
