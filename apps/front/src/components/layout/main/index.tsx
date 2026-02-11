a9dde89c7f34a700befcf7d9903d7e6200315ffa
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-02-11 19:04:56 +0100

    chore: Move navlink and asside-nav

--- apps/front/src/components/layout/main/index.tsx
+++ apps/front/src/components/layout/main/index.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from "@tanstack/react-router";
 import { AssideNav } from "./asside-nav";
+import { MainProviders } from "./asside-nav/providers";
 
 export const MainLayout = () => {
   return (
-    <div className="flex flex-row-reverse min-h-screen">
-      <main className="w-full">
-        <Outlet />
-      </main>
-      <AssideNav />
-    </div>
+    <MainProviders>
+      <div className="flex flex-row-reverse min-h-screen">
+        <main className="w-full">
+          <Outlet />
+        </main>
+        <AssideNav />
+      </div>
+    </MainProviders>
   );
 };
