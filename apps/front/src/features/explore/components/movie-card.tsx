bf52cee415545cc475cb02ff40f551c66fe28f23
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-02-12 19:32:48 +0100

    refactor: Add responsive on moviecard

--- apps/front/src/features/explore/components/movie-card.tsx
+++ apps/front/src/features/explore/components/movie-card.tsx
@@ -13,21 +13,23 @@ export const MovieCard = (props: MovieCardProps) => {
   return (
     <Link
       to={routes.contents.detail(props.movie.id)}
-      className="h-80 aspect-5/8 "
+      className="h-50 sm:h-80 aspect-5/8 "
     >
       {props.movie.posterUrl ? (
         <img
-          className="h-full w-full object-cover rounded-4xl"
+          className="h-full w-full object-cover rounded-3xl sm:rounded-4xl"
           src={props.movie.posterUrl}
           alt={props.movie.title}
         />
       ) : (
-        <span className="h-full w-full bg-neutral-500 rounded-4xl" />
+        <span className="h-full w-full bg-neutral-500 rounded-3xl sm:rounded-4xl" />
       )}
     </Link>
   );
 };
 
 export const MovieCardSkeleton = () => {
-  return <Skeleton className="h-80 rounded-3xl aspect-5/8" />;
+  return (
+    <Skeleton className="h-50 sm:h-80 rounded-3xl sm:rounded-3xl aspect-5/8" />
+  );
 };
