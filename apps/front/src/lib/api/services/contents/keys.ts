65a43c193b210718f7ffee1bc036c1c78bfe9571
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-02-23 22:12:42 +0100

    refactor: Add BASEKEY for contents and categories keys

--- apps/front/src/lib/api/services/contents/keys.ts
+++ apps/front/src/lib/api/services/contents/keys.ts
@@ -1,11 +1,14 @@
 import type { GETContentsIdParams, GETContentsParams } from "@packages/api-sdk";
 
+const BASEKEY = "contents";
 export const contentsKeys = {
   discover: (params: GETContentsParams) => [
+    BASEKEY,
     "discover",
     ...Object.values(params),
   ],
   get: (contentId: string, params?: GETContentsIdParams) => [
+    BASEKEY,
     "get",
     contentId,
     params,
