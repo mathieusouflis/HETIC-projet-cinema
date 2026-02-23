2238d7834bfd277f493dc3d949a65b840de6d059
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-02-23 20:24:25 +0100

    feat: Add query for get content by id

--- apps/front/src/features/content/indext.tsx
+++ apps/front/src/features/content/indext.tsx
@@ -1,9 +1,12 @@
 import { useParams } from "@tanstack/react-router";
+import { getApi } from "@/lib/api/services";
 
 export function ContentPage() {
   const { contentId } = useParams({
     from: "/_main/contents/$contentId/",
   });
-  console.log(contentId);
-  return <div>{contentId}</div>;
+  const { contents } = getApi();
+
+  const { data } = contents.get(contentId);
+  return <div>{JSON.stringify(data)}</div>;
 }
